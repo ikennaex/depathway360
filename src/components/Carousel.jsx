120693ee0185955ef99f25e4b85f1835ddb8a586
ikennaex <ikennaexcel@gmail.com>
2024-05-30 18:56:52 +0100

    Update intro

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,19 +3,14 @@ import "./carousel.css"
 
 const slides = [
   {
-    id: 5,
+    id: 4,
     imgSrc: "https://images.pexels.com/photos/13988500/pexels-photo-13988500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     alt: "Slide 5"
   },
   {
-    id: 4,
-    imgSrc: "https://images.pexels.com/photos/8948348/pexels-photo-8948348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "Slide 4"
-  },
-  {
     id: 3,
-    imgSrc: "https://images.pexels.com/photos/9628110/pexels-photo-9628110.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    alt: "Slide 3"
+    imgSrc: "https://images.pexels.com/photos/9628111/pexels-photo-9628111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Slide 4"
   },
   {
     id: 2,
@@ -24,7 +19,7 @@ const slides = [
   },
   {
     id: 1,
-    imgSrc: "https://images.pexels.com/photos/9628111/pexels-photo-9628111.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    imgSrc: "https://images.pexels.com/photos/8948348/pexels-photo-8948348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     alt: "Slide 1"
   }
 ];
